297daeacc957bd21a4cfa92fbf71d5871195a2ab
Elaine5 <lanne9873@gmail.com>
2022-06-28 15:17:50 -0300

    Inicio

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Aula-JS-Dio
 Aula de javascript pelo bootcamp WebDev
+
+Adicionando o JavaScript no HTML5, deve ser inserido dentro do body e na tag <script>
+
+Pode também referenciar o arquivo .js na tag <head> com <script type e src>
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar")
+}
+
+
+
+
+/* var nome = "Elaine Patricia";
+var idade = 27;
+var idade2 = 1;
+var frase = "China é o melhor time do mundo";
+
+//alert(nome + "tem" + idade + "anos");
+
+//alert("idade + idade2");
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("China", "Alemanha"));
+alert(frase.replace("China", "Alemanha"));
+*/
